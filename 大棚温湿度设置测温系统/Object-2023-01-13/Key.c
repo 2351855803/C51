2605b9c778e97664b663e3bbd37160d2757cea8f
2351855803 <117913146+2351855803@users.noreply.github.com>
2023-01-13 16:55:11 +0800

    Add files via upload

---
+++ 大棚温湿度设置测温系统/Object-2023-01-13/Key.c
@@ -0,0 +1,64 @@
+#include <REGX52.H>
+#include "Delay.h"
+#include "LCD1602.h"
+
+/**
+  *@brief	通过按键来选择模式
+  *@param	
+  *@return	按键代表的模式  
+ */
+unsigned char Matrix_key()
+{
+	unsigned char n,m;
+	P1=0xf0;
+	
+	//检测到有按键按下，高四位会有一个变成0
+	if((P1&0xf0)!=0xf0);
+	{
+		Delayms(10);
+			
+		if((P1&0xf0)!=0xf0);
+		{
+			//取高四位
+			n=P1&0xf0;
+			
+			P1=0x0f;
+
+			//取低四位
+			m=P1&0x0f;
+			
+			return(n+m);
+		}
+	}
+}
+
+/**
+  *@brief	通过矩阵键盘来设置温度湿度上下限
+  *@param	分别是 湿度上限 湿度下线 温度上限 温度下线
+  *@return  void
+ */
+void Matrix_Set_DH(unsigned char * humHWarn, unsigned char * humLWarn,\
+			  unsigned char * tmpHWarn, unsigned char * tmpLWarn)
+{
+	//湿度上下限加减
+	if(0x77 == Matrix_key()) {(*humHWarn)++; while(0x77 == Matrix_key());}
+	if(0x7b == Matrix_key()) {(*humHWarn)--; while(0x7b == Matrix_key());}
+	if(0x7d == Matrix_key()) {(*humLWarn)++; while(0x7d == Matrix_key());}
+	if(0x7e == Matrix_key()) {(*humLWarn)--; while(0x7e == Matrix_key());}
+	
+	//温度上下限加减
+	if(0xb7 == Matrix_key()) {(*tmpHWarn)++; while(0xb7 == Matrix_key());}
+	if(0xbb == Matrix_key()) {(*tmpHWarn)--; while(0xbb == Matrix_key());}
+	if(0xbd == Matrix_key()) {(*tmpLWarn)++; while(0xbd == Matrix_key());}
+	if(0xbe == Matrix_key()) {(*tmpLWarn)--; while(0xbe == Matrix_key());}
+}
+
+
+
+
+
+
+
+
+
+
---
+++ 大棚温湿度设置测温系统/Object-2023-01-13/LCD1602.c
@@ -0,0 +1,180 @@
+#include <REGX52.H>
+#include <intrins.h>
+#include "Delay.h"
+#include "Key.h"
+
+//定义寄存器
+#define Lcdout P0
+
+
+//定义引脚
+sbit RS = P2^6;
+sbit RW = P2^5;
+sbit EN = P2^7;
+
+
+
+//**************************************************************
+//----------------------------  Write_cmd 和 Write_data 差别在RS电平
+//发送命令  RS = 0
+void Write_cmd(unsigned char cmd)
+{
+
+	RS = 0;
+	RW = 0;
+	EN = 0;
+
+	//单片机把要发送的数据放到数据总线里
+	Lcdout = cmd;
+	
+	EN = 1;
+	
+	_nop_();
+
+      EN = 0;
+	
+	Delayms(1);
+
+	
+}
+//**************************************************************
+//发送数据 RS = 1
+void Write_data(unsigned char cmd)
+{
+
+	RS = 1;
+	RW = 0;
+	EN = 0;
+
+	//单片机把要发送的数据放到数据总线里
+	Lcdout = cmd;
+	
+	EN = 1;
+	
+	_nop_();
+
+      EN = 0;
+	
+	Delayms(1);
+	
+
+}
+//**************************************************************
+void Lcd_initial()
+{
+
+	Write_cmd(0x01);
+	Write_cmd(0x06);
+	
+	Write_cmd(0x0c);
+	Write_cmd(0x38);
+	
+
+	
+	Delayms(1);
+	
+	
+}
+//**************************************************************
+void Lcd_write_string(char cmd, char* addr)
+{
+	while(*addr != '\0')
+	{
+		Write_cmd(cmd++);
+		Write_data(*addr);
+		addr++;
+	}
+}
+
+//**************************************************************
+
+//在第几行第几列显示数字
+//形参列表（行，列，数字）
+
+//**************************************************************
+void Lcd_write_number(char line, char row ,unsigned char number)
+{
+	if(1 == line)
+	{
+		Write_cmd(0x80+row-1);
+	
+		Write_data((number/10)+0x30);
+		Write_data((number%10)+0x30);
+	}
+	else if(2 == line)
+	{
+		Write_cmd(0xc0+row-1);
+		
+		Write_data((number/10)+0x30);
+		Write_data((number%10)+0x30);
+	}
+}
+
+/**
+  *@brief	LCD显示开始界面
+  *@param	void
+  *@return	void
+ */
+void LCD_ShowBegin()
+{
+	char i;
+	
+	for(i = 15; i >= 0; i--)
+	{
+		Lcd_write_string(0x80+i,"Welcome to use!");
+		Delayms(300);
+	}
+	for(i = 15; i >= 0; i--)
+	{
+		Lcd_write_string(0xc0+i,"Please set!   ->");
+		Delayms(300);
+	}
+	while(0xee != Matrix_key());
+	Delayms(100);
+	Write_cmd(0x01);
+	Delayms(100);
+	
+	
+}
+
+
+/**
+  *@brief	LCD显示设置温湿度界面
+  *@param	void
+  *@return	void
+ */
+void LCD_ShowSetTH(unsigned char * humHWarn, unsigned char * humLWarn,\
+			  unsigned char * tmpHWarn, unsigned char * tmpLWarn)
+{
+	while(0xee != Matrix_key())
+	{
+		Matrix_Set_DH(humHWarn,humLWarn,tmpHWarn,tmpLWarn);
+		Lcd_write_string(0x80,"tmp:");
+		Lcd_write_string(0xc0,"hum:");
+		Lcd_write_string(0x85,"MAX");
+		
+		Lcd_write_number(1,9,*humHWarn);
+		
+		Lcd_write_string(0x8B,"MIN");		
+		
+		Lcd_write_number(1,15,*humLWarn);
+		
+		Lcd_write_string(0xC5,"MAX");
+		
+		Lcd_write_number(2,9,*tmpHWarn);
+		
+		Lcd_write_string(0xCB,"MIN");
+		
+		Lcd_write_number(2,15,*tmpLWarn);
+	}
+	Write_cmd(0x01);
+	Delayms(1000);
+	
+}
+
+
+
+
+
+
+
